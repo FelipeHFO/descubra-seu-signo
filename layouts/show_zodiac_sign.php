7be6c232f6f62869589cc213357ba28adcee4f73
Felipe Henrique Fonseca Oliveira <felipe.henrique.f.o@gmail.com>
2024-10-27 11:14:45 -0300

    estilizando resultado do signo

--- layouts/show_zodiac_sign.php
+++ layouts/show_zodiac_sign.php
@@ -3,37 +3,40 @@
   <?php include('header.php'); ?>
 
   <body class="container py-5">
-    <h3 class="text-center mb-4">Seu Signo é:</h3>
 
-    <?php
-      $data_nascimento = DateTime::createFromFormat('Y-m-d', $_POST['data_nascimento']);
-      if ($data_nascimento === false) {
-          echo "<p class='text-danger text-center'>Data de nascimento inválida.</p>";
-      } else {
-        $signos = simplexml_load_file("signos.xml");
-        $signoEncontrado = null;
+    <div class="conteudo">
+      <h3 class="text-center mb-4">Seu Signo é:</h3>
 
-        foreach ($signos->signo as $signo) {
-            $dataInicio = DateTime::createFromFormat('d/m', $signo->dataInicio);
-            $dataFim = DateTime::createFromFormat('d/m', $signo->dataFim);
-
-            $dataInicio->setDate($data_nascimento->format('Y'), $dataInicio->format('m'), $dataInicio->format('d'));
-            $dataFim->setDate($data_nascimento->format('Y'), $dataFim->format('m'), $dataFim->format('d'));
-
-            if (($data_nascimento >= $dataInicio && $data_nascimento <= $dataFim) ||
-                ($dataInicio > $dataFim && ($data_nascimento >= $dataInicio || $data_nascimento <= $dataFim))) {
-                $signoEncontrado = $signo;
-                break;
-            }
-        }
-
-        if ($signoEncontrado) {
-            echo "<h4 class='text-center'>{$signoEncontrado->signoNome}</h4>";
-            echo "<p class='text-center'>{$signoEncontrado->descricao}</p>";
+      <?php
+        $data_nascimento = DateTime::createFromFormat('Y-m-d', $_POST['data_nascimento']);
+        if ($data_nascimento === false) {
+            echo "<p class='text-danger text-center'>Data de nascimento inválida.</p>";
         } else {
-            echo "<p class='text-danger text-center'>Signo não encontrado.</p>";
+          $signos = simplexml_load_file("signos.xml");
+          $signoEncontrado = null;
+
+          foreach ($signos->signo as $signo) {
+              $dataInicio = DateTime::createFromFormat('d/m', $signo->dataInicio);
+              $dataFim = DateTime::createFromFormat('d/m', $signo->dataFim);
+
+              $dataInicio->setDate($data_nascimento->format('Y'), $dataInicio->format('m'), $dataInicio->format('d'));
+              $dataFim->setDate($data_nascimento->format('Y'), $dataFim->format('m'), $dataFim->format('d'));
+
+              if (($data_nascimento >= $dataInicio && $data_nascimento <= $dataFim) ||
+                  ($dataInicio > $dataFim && ($data_nascimento >= $dataInicio || $data_nascimento <= $dataFim))) {
+                  $signoEncontrado = $signo;
+                  break;
+              }
+          }
+
+          if ($signoEncontrado) {
+              echo "<h4 class='text-center' id='signo'>{$signoEncontrado->signoNome}</h4>";
+              echo "<p class='text-center' id='descricao'>{$signoEncontrado->descricao}</p>";
+          } else {
+              echo "<p class='text-danger text-center'>Signo não encontrado.</p>";
+          }
         }
-      }
-    ?>
+      ?>
+    </div>
   </body>
 </html>
